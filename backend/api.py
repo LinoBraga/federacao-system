91faa14711beb7998289f05ac6bc8c365e33c764
LinoBraga <joaolinosantosbragalino@gmail.com>
2026-05-23 10:04:24 -0300

    Liberação de site 7

--- backend/api.py
+++ backend/api.py
@@ -87,13 +87,7 @@ app = FastAPI(
 # Configuração do CORS flexível para desenvolvimento e produção na Vercel
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://federacaoparaibanadexadrez-c437h7e1v-linobragas-projects.vercel.app",
-        "https://federacaoparaibanadexadrez-fdug8byz8-linobragas-projects.vercel.app", # O link atual que deu erro
-        "https://federacaoparaibanadexadrez.vercel.app", # Link principal do seu projeto Vercel (se houver)
-        "http://localhost:5173",  
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
